801db75f80ff602b082486375512c6c7ace097b1
Lance Watkins <Lance.Watkins@asu.edu>
2025-01-23 11:11:41 -0600

    code for using python virtual environment and downloading required python packages

--- spopt_reticulate_coolingcenteropt.R
+++ spopt_reticulate_coolingcenteropt.R
@@ -1,2 +1,10 @@
 library(tidyverse)
 library(reticulate)
+
+## Reticulate to start up your virtual python environment
+vrt.env <- "rpython-virtualenv-coolingcenteropt"
+use_virtualenv("rpython-virtualenv-coolingcenteropt")
+## Install python libraries into virtual environment
+python.packages <- c("scipy","spopt","geopandas","pulp","shapely","spaghetti")
+virtualenv_install(vrt.env, python.packages)
+
