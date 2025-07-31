9d91bcb223a01d2b95f0f0b8a3129a71cd9cafa2
Lance Watkins <Lance.Watkins@asu.edu>
2025-07-31 14:30:04 -0500

    finished the R script to format the input csv files that will be used in generate_osrm_matrices.Rmd

--- formating_input_csv_files.R
+++ formating_input_csv_files.R
@@ -34,4 +34,28 @@ existing_locs$type <- "existing cooling center"
 write.csv(existing_locs,paste0(output.dir,"/existing_cooling_centers.csv"), row.names = FALSE)
 
 ## The demand points need to formatted and merged together so they are in one file 
+## In addition to columns for id, longitude,latitude, and type I will also include RelativeRi and Relative_1 columns which are both different weights. I will also include a columnd for the type of demand point (dp_type)
+## RelativeRi will be renamed a_weight and Relative_1 will be renamed b_weight
 
+dp_homelessheatd <- dp_homelessheatd[,c("PointID","POINT_X","POINT_Y","RelativeRi","Relative_1")]
+colnames(dp_homelessheatd) <- c("id","longitude","latitude","a_weight","b_weight")
+dp_homelessheatd$type <- "demand point"
+dp_homelessheatd$dp_type <- 4 ## code for the pop/data sources, higher value (4) indicates most at risk
+
+dp_homelessoutrch <- dp_homelessoutrch[,c("PointID","POINT_X","POINT_Y","RelativeRi","Relative_1")]
+colnames(dp_homelessoutrch) <- c("id","longitude","latitude","a_weight","b_weight")
+dp_homelessoutrch$type <- "demand point"
+dp_homelessoutrch$dp_type <- 3
+
+dp_homelesspit <- dp_homelesspit[,c("PointID","POINT_X","POINT_Y","RelativeRi","Relative_1")]
+colnames(dp_homelesspit) <- c("id","longitude","latitude","a_weight","b_weight")
+dp_homelesspit$type <- "demand point"
+dp_homelesspit$dp_type <- 2
+
+dp_ressample <- dp_ressample[,c("PointID","POINT_X","POINT_Y","RelativeRi","Relative_1")]
+colnames(dp_ressample) <- c("id","longitude","latitude","a_weight","b_weight")
+dp_ressample$type <- "demand point"
+dp_ressample$dp_type <- 1
+
+demand_pts <- rbind(dp_homelessheatd,dp_homelessoutrch,dp_homelesspit,dp_ressample)
+write.csv(demand_pts,paste0(output.dir,"/demand_points.csv"), row.names = FALSE)
